1ac8cf2a79eed179fc3c31340003e14d8959b238
Cichol <cichol@live.cn>
2015-11-09 14:06:57 +0800

    insert preview css

--- vendor/assets/javascripts/kindeditor/plugins/preview/preview.js
+++ vendor/assets/javascripts/kindeditor/plugins/preview/preview.js
@@ -12,18 +12,20 @@ KindEditor.plugin('preview', function(K) {
 	self.clickToolbar(name, function() {
 		var lang = self.lang(name + '.'),
 			html = '<div style="padding:10px 20px;">' +
-				'<iframe class="ke-textarea" frameborder="0" style="width:708px;height:400px;"></iframe>' +
+				'<iframe class="ke-textarea" frameborder="0" style="width:851px;height:400px;"></iframe>' +
 				'</div>',
 			dialog = self.createDialog({
 				name : name,
-				width : 750,
+				width : 892,
 				title : self.lang(name),
 				body : html
 			}),
 			iframe = K('iframe', dialog.div),
 			doc = K.iframeDoc(iframe);
 		doc.open();
-		doc.write(self.fullHtml());
+		doc.write('<link rel="stylesheet" type="text/css" href="/editor_preview.css" />');
+		var html = '<div class="plain_box">' + self.html() + '</div>';
+		doc.write(html);
 		doc.close();
 		K(doc.body).css('background-color', '#FFF');
 		iframe[0].contentWindow.focus();
